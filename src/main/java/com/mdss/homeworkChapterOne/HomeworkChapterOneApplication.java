660bc65425c65dc2391f0abbe863abc74ae72f03
Marcelo <marcelossoares@live.com>
2022-07-24 03:52:31 -0300

    first commit, Client entity

---
+++ src/main/java/com/mdss/homeworkChapterOne/HomeworkChapterOneApplication.java
@@ -0,0 +1,13 @@
+package com.mdss.homeworkChapterOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkChapterOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkChapterOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdss/homeworkChapterOne/HomeworkChapterOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdss.homeworkChapterOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkChapterOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
